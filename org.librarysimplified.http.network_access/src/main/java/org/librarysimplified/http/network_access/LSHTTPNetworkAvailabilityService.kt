b2d75d5cdb40318e807621084f28fe3ec9a7e3f8
Mark Raynsford <code@io7m.com>
2026-03-04 16:08:43 +0000

    Eagerly fetch network state.

--- org.librarysimplified.http.network_access/src/main/java/org/librarysimplified/http/network_access/LSHTTPNetworkAvailabilityService.kt
+++ org.librarysimplified.http.network_access/src/main/java/org/librarysimplified/http/network_access/LSHTTPNetworkAvailabilityService.kt
@@ -52,36 +52,66 @@ class LSHTTPNetworkAvailabilityService : Service() {
 
         val cellularCallback =
           object : ConnectivityManager.NetworkCallback() {
+            override fun onCapabilitiesChanged(
+              network: Network,
+              networkCapabilities: NetworkCapabilities,
+            ) {
+              super.onCapabilitiesChanged(network, networkCapabilities)
+              if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)) {
+                LSHTTPNetworkAccess.setCellularAvailable(true)
+              } else {
+                LSHTTPNetworkAccess.setCellularAvailable(false)
+              }
+            }
+
             override fun onAvailable(network: Network) {
               super.onAvailable(network)
+              this@LSHTTPNetworkAvailabilityService.logger.debug("Cellular became available.")
               LSHTTPNetworkAccess.setCellularAvailable(true)
             }
 
             override fun onLost(network: Network) {
               super.onLost(network)
+              this@LSHTTPNetworkAvailabilityService.logger.debug("Cellular became unavailable (Lost).")
               LSHTTPNetworkAccess.setCellularAvailable(false)
             }
 
             override fun onUnavailable() {
               super.onUnavailable()
+              this@LSHTTPNetworkAvailabilityService.logger.debug("Cellular became unavailable (Unavailable).")
               LSHTTPNetworkAccess.setCellularAvailable(false)
             }
           }
 
         val wifiCallback =
           object : ConnectivityManager.NetworkCallback() {
+            override fun onCapabilitiesChanged(
+              network: Network,
+              networkCapabilities: NetworkCapabilities,
+            ) {
+              super.onCapabilitiesChanged(network, networkCapabilities)
+              if (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)) {
+                LSHTTPNetworkAccess.setWIFIAvailable(true)
+              } else {
+                LSHTTPNetworkAccess.setWIFIAvailable(false)
+              }
+            }
+
             override fun onAvailable(network: Network) {
               super.onAvailable(network)
+              this@LSHTTPNetworkAvailabilityService.logger.debug("WIFI became available.")
               LSHTTPNetworkAccess.setWIFIAvailable(true)
             }
 
             override fun onLost(network: Network) {
               super.onLost(network)
+              this@LSHTTPNetworkAvailabilityService.logger.debug("WIFI became unavailable (Lost).")
               LSHTTPNetworkAccess.setWIFIAvailable(false)
             }
 
             override fun onUnavailable() {
               super.onUnavailable()
+              this@LSHTTPNetworkAvailabilityService.logger.debug("WIFI became unavailable (Unavailable).")
               LSHTTPNetworkAccess.setWIFIAvailable(false)
             }
           }
@@ -91,6 +121,8 @@ class LSHTTPNetworkAvailabilityService : Service() {
 
         connectivityManager.requestNetwork(wifiRequest, wifiCallback)
         connectivityManager.requestNetwork(cellularRequest, cellularCallback)
+
+        this.determineInitialNetworkState(connectivityManager)
       } catch (e: Throwable) {
         this.logger.debug("Failed to start network service: ", e)
         isRunning.set(false)
@@ -99,6 +131,49 @@ class LSHTTPNetworkAvailabilityService : Service() {
       this.logger.debug("Ignoring redundant request to start network service.")
     }
     return super.onStartCommand(intent, flags, startId)
+  }
+
+  private fun determineInitialNetworkState(
+    connectivityManager: ConnectivityManager,
+  ) {
+    var cellularAvailable = false
+    var wifiAvailable = false
+
+    try {
+      val active = connectivityManager.activeNetwork
+      if (active != null) {
+        val caps = connectivityManager.getNetworkCapabilities(active)
+        if (caps != null) {
+          if (caps.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
+            caps.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
+          ) {
+            wifiAvailable =
+              caps.hasTransport(NetworkCapabilities.TRANSPORT_WIFI)
+            cellularAvailable =
+              caps.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR)
+          }
+        }
+      }
+    } catch (e: Throwable) {
+      this.logger.debug("Initial network state: Failed: ", e)
+      cellularAvailable = true
+      wifiAvailable = true
+    }
+
+    if (wifiAvailable) {
+      this.logger.debug("Initial network state: WIFI AVAILABLE")
+    } else {
+      this.logger.debug("Initial network state: WIFI UNAVAILABLE")
+    }
+
+    if (cellularAvailable) {
+      this.logger.debug("Initial network state: Cellular AVAILABLE")
+    } else {
+      this.logger.debug("Initial network state: Cellular UNAVAILABLE")
+    }
+
+    LSHTTPNetworkAccess.setWIFIAvailable(wifiAvailable)
+    LSHTTPNetworkAccess.setCellularAvailable(cellularAvailable)
   }
 
   override fun onBind(intent: Intent?): IBinder? {
